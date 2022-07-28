7984995f9143390810c3c47ad39f80b02aa17670
Yoo <edel1212@naver.com>
2022-07-28 22:47:34 +0900

    JDBC를 이용한 인증 시작- 테이블 생성 및 JDBC 연결완료

--- ex04/src/test/java/org/zerock/persistence/JDBCTests.java
+++ ex04/src/test/java/org/zerock/persistence/JDBCTests.java
@@ -14,23 +14,16 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import lombok.extern.log4j.Log4j;
 
-@RunWith(SpringJUnit4ClassRunner.class) //테스트시 필요한 클래스를 지정, pom의 spring-test가 추가되어있어야 한다!
+@RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")//어떤 설정 정보를 읽어 들여야 하는지를 명시한다
 @Log4j
-public class DataSourceTests {
+public class JDBCTests {
 	@Autowired
 	private DataSource dataSource;
 	
 	@Autowired
 	private SqlSessionFactory sqlSessionFactory;
 	
-	/**
-	 * @description : 1) dataSource 확인은 JDBC 연결 확인이 선행 된 후 진행 해야한다!
-	 * 				  
-	 *                2) JUNIT test를 하기 위해서는
-	 *                프로젝트 library에 JUNIT을 추가해주어야 한다!
-	 *                프포젝트 우클릭 -> properties -> java build path -> library -> add 해주기!
-	 * */
 	@Test
 	public void testConnection() {
 		try {
@@ -52,5 +45,4 @@ public class DataSourceTests {
 			e.printStackTrace();
 		}
 	}
-	
 }
